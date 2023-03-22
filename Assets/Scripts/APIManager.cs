bd266c7623aa41f0a233e6c9a07072c3862fff5e
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-03-22 12:04:41 +0100

    call api/ handle response/ use in unity

--- Assets/Scripts/APIManager.cs
+++ Assets/Scripts/APIManager.cs
@@ -2,29 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
+
+[System.Serializable] struct Parameter
+{
+    public string name;
+    public string value;
+}
 
 public class APIManager : MonoBehaviour
 {
+    [SerializeField]string _endpoint;
+    [SerializeField]List<Parameter> _parameters;
+    string queryString;
+    Color newColor;
+    [SerializeField] MeshRenderer sphereMat;
+    [SerializeField] Image button;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
     public void CallURL()
     {
-        StartCoroutine(GetRequest("https://webhook.site/11805f45-e44d-4230-b0db-5eab8dda93f7?question=combien%20de%20giga&objet=forfait"));
+        // foreach( Parameter param in _parameters){
+        //     queryString += param.name + "=" + param.value + "&";
+        // }
+        // if(queryString.EndsWith('&'))
+        // {
+        //     queryString.Remove(queryString.Length-1);
+        // }
+        for (int i = 0; i < _parameters.Count; i++)
+        {
+            if (i == 0)
+            {
+                queryString += "?";
+            } else 
+            {
+                queryString += "&";
+            }
+            queryString += _parameters[i].name + "=" + _parameters[i].value;
+
+        }
+        string requestURL = _endpoint + queryString;
+        Debug.Log(requestURL);
+        StartCoroutine(GetRequest(requestURL));
     }
     IEnumerator GetRequest(string uri)
     {
         using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
         {
             yield return webRequest.SendWebRequest();
+            if(webRequest.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log("Il y a un problème de connexion");
+            } else
+            {
+                HandleResponse(webRequest);
+            }
         }
+    }
+    void HandleResponse(UnityWebRequest webRequest){
+        Debug.Log(webRequest.downloadHandler.text);
+        ColorUtility.TryParseHtmlString(webRequest.downloadHandler.text, out newColor);
+        sphereMat.material.color = newColor;
+        button.color = newColor;
     }
 }
